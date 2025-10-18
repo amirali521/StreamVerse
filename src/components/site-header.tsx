f473817778934d4e9422c4ec69d777adf543a46f
Amir Ali <hacktech430@gmail.com>
2025-10-18 17:22:50 +0000

    Add left side padding to hamburger icon and increase its size little

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -102,9 +102,9 @@ function MobileNav({ open, setOpen }: { open: boolean, setOpen: (open: boolean) 
       <SheetTrigger asChild>
         <Button
           variant="ghost"
-          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+          className="mr-2 px-0 pl-2 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
         >
-          <Menu className="h-6 w-6" />
+          <Menu className="h-7 w-7" />
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
