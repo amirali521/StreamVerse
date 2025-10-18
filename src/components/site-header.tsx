f7a040560c9edecf07bdd137c42d097004d2e030
Amir Ali <hacktech430@gmail.com>
2025-10-18 17:33:24 +0000

    You need to use image url as logo link I will provide PNG link or upload

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -21,23 +21,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { useState } from "react";
-
-function PlayReelIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      {...props}
-    >
-      <path
-        fillRule="evenodd"
-        d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zM4 12c0-.9.18-1.75.5-2.54V10h2.14c.2-.84.48-1.63.83-2.36L5.61 5.61A8.96 8.96 0 014 12zm1.61-6.39l1.86 1.86c.73-.35 1.52-.63 2.36-.83V4.5c-.79.32-1.55.7-2.22 1.11zM10 4.5v2.14c.64-.13 1.3-.21 1.99-.21v-2a9.01 9.01 0 00-1.99.07zM14 4.55v2.02c.69 0 1.35-.08 2 .21V4.5A9.01 9.01 0 0014 4.55zm3.11 2.06c.35.73.63 1.52.83 2.36H20v-.96c-.32-.79-.7-1.55-1.11-2.22l-1.78 1.82zM10 10.5v3l2.25-1.5L10 10.5zm9.5 0v.96c.13.64.21 1.3.21 2.01h2.02c0-.69-.08-1.35-.21-2h-2.02zm-.21 4.01c-.13.64-.29 1.26-.5 1.84h2.21v-.96a8.95 8.95 0 01-.5-2.84zm-4.18 2.38c-.73.35-1.52.63-2.36.83v2.14c.79-.32 1.55-.7 2.22-1.11l.14-.14zm-4.44.83c-.64.13-1.3.21-1.99.21v2c.69 0 1.35-.08 2-.21v-2zm-3.8-2.29c-.35-.73-.63-1.52-.83-2.36H4v.96c.32.79.7 1.55 1.11 2.22l1.78-1.82z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
+import Image from "next/image";
 
 function UserNav() {
   const { app } = useFirebase();
@@ -187,7 +171,7 @@ export function SiteHeader() {
         <MobileNav open={open} setOpen={setOpen} />
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <PlayReelIcon className="h-6 w-6 text-primary" />
+            <Image src="https://placehold.co/32x32/png?text=SV" alt="StreamVerse Logo" width={32} height={32} />
             <span className="font-bold font-headline text-xl">StreamVerse</span>
           </Link>
           <nav className="hidden md:flex items-center gap-6 text-sm">
