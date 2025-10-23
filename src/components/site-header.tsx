1e5337366039269cd46bc092380bf2db99bc3b4f
Amir Ali <hacktech430@gmail.com>
2025-10-23 11:26:01 +0000

    Add left right side padding in header 4px

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -235,7 +235,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-1">
         <MobileNav open={open} setOpen={setOpen} navItems={navItems} />
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
