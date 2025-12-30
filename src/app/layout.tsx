ed25933e82b64a315e19549baa558b941a9a7af7
Amir Ali <hacktech430@gmail.com>
2025-12-30 18:47:01 +0000

    As I give you the details of vidlinks page what they want me to setup I

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { FirebaseClientProvider } from "@/firebase/client-provider";
 import { WatchPageLayout } from "@/components/watch-page-layout";
 import { AdProvider } from "@/context/ad-provider";
 import { AdInterstitial } from "@/components/ad-interstitial";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "StreamVerse",
@@ -22,6 +23,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1111111111"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1111111111');
+          `}
+        </Script>
+        <meta name="monetag" content="0208fe0f0196da351b14246835a2460b" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
