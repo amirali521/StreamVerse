f85ca03823fceab6f11d5dfa4acdea8823f2cf01
Amir Ali <hacktech430@gmail.com>
2026-01-10 08:54:09 +0000

    As you can see the floating link in image if it appears from the one sid

--- src/components/floating-link.tsx
+++ src/components/floating-link.tsx
@@ -6,37 +6,15 @@ import { Button } from "./ui/button";
 
 export function FloatingLink() {
   return (
-    <div className="relative my-8 h-14 overflow-hidden bg-background">
+    <div className="relative my-8 h-14 overflow-hidden bg-transparent">
       <div className="absolute inset-0 flex items-center">
         <div className="w-full flex justify-start animate-marquee whitespace-nowrap">
-            <Button asChild variant="link" className="text-lg font-bold text-accent hover:text-accent/80 mx-8">
+            <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8">
                 <Link href="/servers">
                     Looking for more? Click here to search our external servers for any movie or series!
                 </Link>
             </Button>
-            <Button asChild variant="link" className="text-lg font-bold text-accent hover:text-accent/80 mx-8">
-                <Link href="/servers">
-                    Looking for more? Click here to search our external servers for any movie or series!
-                </Link>
-            </Button>
-            <Button asChild variant="link" className="text-lg font-bold text-accent hover:text-accent/80 mx-8">
-                <Link href="/servers">
-                    Looking for more? Click here to search our external servers for any movie or series!
-                </Link>
-            </Button>
-        </div>
-        <div className="absolute top-0 w-full flex justify-start animate-marquee2 whitespace-nowrap">
-             <Button asChild variant="link" className="text-lg font-bold text-accent hover:text-accent/80 mx-8">
-                <Link href="/servers">
-                    Looking for more? Click here to search our external servers for any movie or series!
-                </Link>
-            </Button>
-            <Button asChild variant="link" className="text-lg font-bold text-accent hover:text-accent/80 mx-8">
-                <Link href="/servers">
-                    Looking for more? Click here to search our external servers for any movie or series!
-                </Link>
-            </Button>
-            <Button asChild variant="link" className="text-lg font-bold text-accent hover:text-accent/80 mx-8">
+            <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8">
                 <Link href="/servers">
                     Looking for more? Click here to search our external servers for any movie or series!
                 </Link>
