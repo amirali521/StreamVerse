8f67ddbff19b9e7c73c536469703514c66eb7dad
Amir Ali <hacktech430@gmail.com>
2026-01-10 08:59:20 +0000

    Can you see a little ui problem with link

--- src/components/floating-link.tsx
+++ src/components/floating-link.tsx
@@ -8,12 +8,12 @@ export function FloatingLink() {
   return (
     <div className="relative my-8 flex h-14 overflow-x-hidden bg-transparent">
       <div className="animate-marquee whitespace-nowrap flex items-center">
-        <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8">
+        <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8 whitespace-nowrap">
           <Link href="/servers">
             Looking for more? Click here to search our external servers for any movie or series!
           </Link>
         </Button>
-        <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8">
+        <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8 whitespace-nowrap">
           <Link href="/servers">
             Looking for more? Click here to search our external servers for any movie or series!
           </Link>
@@ -21,12 +21,12 @@ export function FloatingLink() {
       </div>
 
       <div className="absolute top-0 animate-marquee2 whitespace-nowrap flex items-center">
-         <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8">
+         <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8 whitespace-nowrap">
           <Link href="/servers">
             Looking for more? Click here to search our external servers for any movie or series!
           </Link>
         </Button>
-        <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8">
+        <Button asChild variant="link" className="text-lg font-bold text-white hover:text-white/80 mx-8 whitespace-nowrap">
           <Link href="/servers">
             Looking for more? Click here to search our external servers for any movie or series!
           </Link>
