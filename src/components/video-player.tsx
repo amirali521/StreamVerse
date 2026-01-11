d6f478c76741fc78cc6d42aa103fa4ac6012663f
Amir Ali <hacktech430@gmail.com>
2026-01-11 12:32:30 +0000

    So then revert the last change of ads blocking on external server page i

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -75,7 +75,6 @@ export function VideoPlayer({ source, poster }: VideoPlayerProps) {
                     frameBorder="0"
                     allow="autoplay; fullscreen; picture-in-picture"
                     allowFullScreen
-                    sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
                     onLoad={handleLoad}
                     onError={handleError}
                 ></iframe>
